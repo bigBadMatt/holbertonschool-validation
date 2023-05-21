5c96718daa2cb6fca2c5837b920cb5d35005337a
Matt <5707@holbertonstudents.com>
2023-05-21 23:33:47 +1000

    added missing setup.sh commands

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 apt-get update && apt-get install -y make git wget
+
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+
+sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
