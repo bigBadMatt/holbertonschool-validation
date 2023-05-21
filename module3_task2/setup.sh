147b9f0a34ef7642a77960ca7fdeaed58881ea89
Matt <5707@holbertonstudents.com>
2023-05-21 23:36:00 +1000

    added missing setup.sh commands

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install -y make git wget
+apt-get update && apt-get install -y make git wget zip
 
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
