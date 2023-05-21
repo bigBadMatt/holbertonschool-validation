bec97efbf52d83857cf61e2032b6521d2b436498
Matt <5707@holbertonstudents.com>
2023-05-22 09:55:51 +1000

    added node.js to setup.sh

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -4,11 +4,11 @@ apt-get update && apt-get install -y make git wget zip
 wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
 dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash - \
-	&& apt-get install -y --no-install-recommends \
-	nodejs=14.* \
+#curl -sL https://deb.nodesource.com/setup_14.x | bash - \
+#	&& apt-get install -y --no-install-recommends \
+#	nodejs=14.* \
 	# Cleanup APT cache to ease extension of this image
-	&& apt-get clean \
+#	&& apt-get clean \
 
  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
  npm install -g markdownlint-cli
